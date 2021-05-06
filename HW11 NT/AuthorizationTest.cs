6603d1f060f5c85a41980eb6d525c830437c069b
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-05-06 16:13:33 +0300

    UserAuthorizationTest

--- HW11 NT/AuthorizationTest.cs
+++ HW11 NT/AuthorizationTest.cs
@@ -9,46 +9,67 @@ using WebDriverManager.Helpers;
    
 namespace HW11_NT
 {
-    class AuthorizationTest
+    public class UserAuthorizationTest
     {
-        public class UserAuthorizationTest
+        private IWebDriver _webDriver;
+        [SetUp]
+        public void Setup()
         {
-            private IWebDriver _webDriver;
-            [SetUp]
-            public void Setup()
-            {
-                new DriverManager().SetUpDriver(new FirefoxConfig());
-                _webDriver = new FirefoxDriver();
-                _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
-                _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+            _webDriver = new FirefoxDriver();
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
+            _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
 
-            }
-            [TearDown]
-            public void TearDown()
-            {
-                _webDriver.Quit();
-            }
-            [Test]
-            public void RegistrationPartOne()
-            {
-                //email  WillSmith@gmail.com  Oksana89pys@gmail.com
-                //password 123Qwe@321  Oks@n@89
-
-                _webDriver.Navigate().GoToUrl("https://newbookmodels.com/auth/signin");
-
-                _webDriver.FindElement(By.CssSelector("[name = email]")).SendKeys("WillSmith@gmail.com");
-
-                _webDriver.FindElement(By.CssSelector("[name = password]")).SendKeys("123Qwe@321");
-
-                _webDriver.FindElement(By.CssSelector(".SignInForm__submitButton--cUdOV")).Click();
-
-                var result = _webDriver.Url;
-
-                Thread.Sleep(3000);
-
-                Assert.AreEqual("https://newbookmodels.com/explore", result);
-            
-            }
         }
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+        [Test]
+        public void CheckSuccessfulUserAuthorization()
+        {
+            //email  WillSmith@gmail.com  Oksana89pys@gmail.com
+            //password 123Qwe@321  Oks@n@89
+
+            _webDriver.Navigate().GoToUrl("https://newbookmodels.com/auth/signin");
+
+            _webDriver.FindElement(By.CssSelector("[name = email]")).SendKeys("WillSmith@gmail.com");
+
+            _webDriver.FindElement(By.CssSelector("[name = password]")).SendKeys("123Qwe@321");
+
+            _webDriver.FindElement(By.CssSelector(".SignInForm__submitButton--cUdOV")).Click();
+
+            var result = _webDriver.Url;
+
+            Thread.Sleep(3000);
+
+            Assert.AreEqual("https://newbookmodels.com/explore", result);
+        
+        }
+        [Test]
+        public void CheckUserAccountBlockMessage()
+        {
+            //email  WillSmith@gmail.com  Oksana89pys@gmail.com
+            //password 123Qwe@321  Oks@n@89
+
+            _webDriver.Navigate().GoToUrl("https://newbookmodels.com/auth/signin");
+
+            _webDriver.FindElement(By.CssSelector("[name = email]")).SendKeys("WillSmith@gmail.com");
+
+            _webDriver.FindElement(By.CssSelector("[name = password]")).SendKeys("123Qwe@321");
+
+            _webDriver.FindElement(By.CssSelector(".SignInForm__submitButton--cUdOV")).Click();
+
+
+            Thread.Sleep(3000);
+
+            var actualMessage = By.CssSelector("div.FormErrorText__error---nzyq:nth-child(1) > div:nth-child(1)");
+
+            Assert.AreEqual("User account is blocked.", actualMessage);
+
+
+        }
+
     }
 }
---
+++ HW11 NT/UserAuthorizationTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace HW11_NT
+{
+    public class UserAuthorizationTests
+    {
+        private IWebDriver _webDriver;
+        [SetUp]
+        public void Setup()
+        {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+            _webDriver = new FirefoxDriver();
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
+            _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+        [Test]
+        public void RegistrationPartOne()
+        {
+           
+            _webDriver.Navigate().GoToUrl("https://newbookmodels.com/join/");
+
+            _webDriver.FindElement(By.CssSelector("[name = first_name]")).SendKeys("Will");
+
+            _webDriver.FindElement(By.CssSelector("[name = last_name]")).SendKeys("Smith");
+
+            _webDriver.FindElement(By.CssSelector("[name = email]")).SendKeys($"sosixo{_email}5@quossum.com");
+
+            _webDriver.FindElement(By.CssSelector("[name = phone_number]")).SendKeys("4444444444");
+
+            _webDriver.FindElement(By.CssSelector("[name = password]")).SendKeys("@123Will@");
+
+            _webDriver.FindElement(By.CssSelector("[name = password_confirm]")).SendKeys("@123Will@");
+
+            _webDriver.FindElement(By.CssSelector(".SignupForm__submitButton--1m1C2")).Click();
+
+            var result = _webDriver.Url;
+
+            Thread.Sleep(3000);
+
+            Assert.AreEqual("https://newbookmodels.com/join/company", result);
+
+        }
+    }
+}
